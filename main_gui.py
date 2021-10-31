da1be82b12f107955a1b04cc7895502cf7996891
ioxv <ioooxxv@gmail.com>
2021-11-01 01:16:42 +0800

    fix bugs

--- main_gui.py
+++ main_gui.py
@@ -15,6 +15,35 @@ def get_rect(strings="", name=""):  # to find QLineEdit location
     return [int(x), int(y), int(width), int(height)]
 
 
+def save_config(config):
+    with open('main.ui', 'r') as f:
+        ui_config = f.read()
+    ui_config = re.sub(r'( name="bn_path">[\s\S]*?name="text">[\s\S]*?<string>).*?(</string>)', rf"\1{config['bn']}\2",
+                       ui_config)
+    ui_config = re.sub(r'( name="log_path">[\s\S]*?name="text">[\s\S]*?<string>).*?(</string>)',
+                       rf"\1{config['log']}\2", ui_config)
+    ui_config = re.sub(r'( name="team_id">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['team_id']}\2", ui_config)
+    ui_config = re.sub(r'( name="reward_count">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['reward_count']}\2", ui_config)
+    ui_config = re.sub(r'( name="hero_count">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['hero_count']}\2", ui_config)
+    ui_config = re.sub(r'( name="language">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['language']}\2", ui_config)
+    ui_config = re.sub(r'( name="boss_id">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['boss_id']}\2", ui_config)
+    ui_config = re.sub(r'( name="longest_waiting">[\s\S]*?name="value">[\s\S]*?<number).*?(</number>)',
+                       rf"\1>{config['longest_waiting']}\2", ui_config)
+    ui_config = re.sub(r'( name="delay">[\s\S]*?name="value">[\s\S]*?<double).*?(</double>)',
+                       rf"\1>{config['delay']}\2", ui_config)
+    ui_config = re.sub(r'( name="auto_restart">[\s\S]*?name="checked">[\s\S]*?<bool).*?(</bool>)',
+                       rf"\1>{'true' if config['auto_restart'] else 'false'}\2", ui_config)
+    ui_config = re.sub(r'( name="early_stop">[\s\S]*?name="checked">[\s\S]*?<bool).*?(</bool>)',
+                       rf"\1>{'true' if config['early_stop'] else 'false'}\2", ui_config)
+    with open('main.ui', 'w') as f:
+        f.writelines(ui_config)
+
+
 class DropLineEdit(QLineEdit):  # get file path by using mouse
     def __init__(self, parent=None):
         super(DropLineEdit, self).__init__(parent)
@@ -53,19 +82,8 @@ class Ui(QMainWindow):
         with open('main.ui', 'r') as f:
             ui_xml = f.read().replace('\n', "")
 
-        log_rect = get_rect(ui_xml, "log_path")
         self.log = self.findChild(QLineEdit, 'log_path')
-        # self.input.setAcceptDrops(True)
-        self.log = DropLineEdit(self)
-        self.log.setGeometry(QtCore.QRect(*log_rect))
-        self.log.setPlaceholderText("Please input Power.log path")
-
-        bn_rect = get_rect(ui_xml, "bn_path")
         self.bn = self.findChild(QLineEdit, 'bn_path')
-        self.bn = DropLineEdit(self)
-        self.bn.setGeometry(QtCore.QRect(*bn_rect))
-        self.bn.setPlaceholderText("Please input Battle.net.exe path")
-
         self.language = self.findChild(QSpinBox, 'language')
         self.team_id = self.findChild(QSpinBox, 'team_id')
         self.boss_id = self.findChild(QSpinBox, "boss_id")
@@ -73,24 +91,53 @@ class Ui(QMainWindow):
         self.reward_count = self.findChild(QSpinBox, "reward_count")
         self.delay = self.findChild(QDoubleSpinBox, "delay")
         self.confidence = self.findChild(QDoubleSpinBox, "confidence")
-        self.longest_waiting = self.findChild(QDoubleSpinBox, "longest_waiting")
+        self.longest_waiting = self.findChild(QSpinBox, "longest_waiting")
         self.early_stop = self.findChild(QCheckBox, "early_stop")
         self.auto_restart = self.findChild(QCheckBox, "auto_restart")
+
+        log_rect = get_rect(ui_xml, "log_path")
+        # self.input.setAcceptDrops(True)
+        default_log = self.log.text()
+        if default_log:
+            self.log = DropLineEdit(self)
+            self.log.setGeometry(QtCore.QRect(*log_rect))
+            self.log.setText(default_log)
+        else:
+            self.log = DropLineEdit(self)
+            self.log.setGeometry(QtCore.QRect(*log_rect))
+            self.log.setPlaceholderText("Please input Power.log path")
+
+        bn_rect = get_rect(ui_xml, "bn_path")
+        default_bn = self.bn.text()
+        if default_bn:
+            self.bn = DropLineEdit(self)
+            self.bn.setGeometry(QtCore.QRect(*bn_rect))
+            self.bn.setText(default_bn)
+        else:
+            self.bn = DropLineEdit(self)
+            self.bn.setGeometry(QtCore.QRect(*bn_rect))
+            self.bn.setPlaceholderText("Please input Battle.net.exe path")
 
         self.show()
 
     def saveButtonPressed(self):
         # This is executed when the button is pressed
-        print('Power.log:\t' + self.log.text())
-        print('Battle.net:\t' + self.bn.text())
-        print("early_stop:\t", end="")
-        print(self.early_stop.isChecked())
-        print("auto_restart:\t", end="")
-        print(self.auto_restart.isChecked())
-        print("delay:\t", end="")
-        print(self.delay.value())
-        print("boss_id:\t", end="")
-        print(self.boss_id.value())
+        self.basic_config = {
+            'log': self.log.text(),
+            'bn': self.bn.text(),
+            'early_stop': self.early_stop.isChecked(),
+            'auto_restart': self.auto_restart.isChecked(),
+            'team_id': self.team_id.value(),
+            'language': self.language.value(),
+            'boss_id': self.boss_id.value(),
+            'hero_count': self.hero_count.value(),
+            'reward_count': self.reward_count.value(),
+            'delay': self.delay.value(),
+            'confidence': self.confidence.value(),
+            'longest_waiting': self.longest_waiting.value()
+        }
+        print(self.basic_config)
+        save_config(self.basic_config)
 
     def runButtonPressed(self):
         print('run')
---
+++ start_gui.bat
@@ -0,0 +1,1 @@
+start pythonw main_gui.py
