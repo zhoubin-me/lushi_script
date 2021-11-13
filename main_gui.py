0db8a7cd2f60feb7b4c9d60c0272b102a96e1bd4
blacksilvergeek <46051884+blacksilvergeek@users.noreply.github.com>
2021-11-13 21:11:13 +0800

    Add files via upload

--- main_gui.py
+++ main_gui.py
@@ -8,6 +8,10 @@ import sys
 import threading
 import traceback
 
+import sip
+
+from utils.extendedcombobox import ExtendedComboBox
+
 import PyQt5
 import keyboard
 import pinyin
@@ -16,6 +20,7 @@ from PyQt5 import uic, QtCore, QtWidgets
 from PyQt5.QtCore import QStringListModel, Qt, QSortFilterProxyModel
 from PyQt5.QtWidgets import *
 
+#from spyderlib.qt.QtGui.QComboBox import setSizeAdjustPolicy
 
 
 from utils.util import HEROS
@@ -46,49 +51,7 @@ def stop_thread(thread):
     _async_raise(thread.ident, SystemExit)
 
 
-class ExtendedComboBox(QComboBox):
-    def __init__(self, parent=None):
-        super(ExtendedComboBox, self).__init__(parent)
 
-        self.setFocusPolicy(Qt.StrongFocus)
-        self.setEditable(True)
-
-        # add a filter model to filter matching items
-        self.pFilterModel = QSortFilterProxyModel(self)
-        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        self.pFilterModel.setSourceModel(self.model())
-
-        # add a completer, which uses the filter model
-        self.completer = QCompleter(self.pFilterModel, self)
-        # always show all (filtered) completions
-        self.completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
-        self.setCompleter(self.completer)
-
-        # connect signals
-        self.lineEdit().textEdited.connect(self.pFilterModel.setFilterFixedString)
-        self.completer.activated.connect(self.on_completer_activated)
-
-
-    # on selection of an item from the completer, select the corresponding item from combobox 
-    def on_completer_activated(self, text):
-        if text:
-            index = self.findText(text)
-            self.setCurrentIndex(index)
-            self.activated[str].emit(self.itemText(index))
-
-
-    # on model change, update the models of the filter and completer as well 
-    def setModel(self, model):
-        super(ExtendedComboBox, self).setModel(model)
-        self.pFilterModel.setSourceModel(model)
-        self.completer.setModel(self.pFilterModel)
-
-
-    # on model column change, update the model column of the filter and completer as well
-    def setModelColumn(self, column):
-        self.completer.setCompletionColumn(column)
-        self.pFilterModel.setFilterKeyColumn(column)
-        super(ExtendedComboBox, self).setModelColumn(column)  
 
 
 class Ui(QMainWindow):
@@ -126,9 +89,12 @@ class Ui(QMainWindow):
         self.run.clicked.connect(self.runButtonPressed)  # Click event
         self.load = self.findChild(QPushButton, 'load')  # Find the button
         self.load.clicked.connect(self.loadButtonPressed)  # Click event
+
+
+        self.hero_dropdown=(self.findChild(ExtendedComboBox, 'hero_list'))
+       
         
-        self.hero_dropdown=ExtendedComboBox(self.findChild(QComboBox, 'hero_list'))
-        self.hero_dropdown.resize(200,30)
+        
         if self.ui_lang == 'chs':
             heroes_sorted = {k: v[0] for k, v in sorted(
                 HEROS.items(), key=lambda item: pinyin.get(item[1][0], format="strip", delimiter=""))}
@@ -178,6 +144,9 @@ class Ui(QMainWindow):
         self.hero_info = {}
         self.config = {}
         self.load_config('config/default.yaml')
+
+
+
         self.show()
 
         if self.ui_lang == 'eng':
