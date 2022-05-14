36eaf0309c2e1b6d7c2b86e894cbd4f9ab0d3c09
littlewrite <773821422@qq.com>
2022-05-14 20:56:57 +0800

    报错也继续运行

--- cn_update.bat
+++ cn_update.bat
@@ -1,3 +1,3 @@
-git checkout main
-git pull origin main
+git checkout main || true
+git pull origin main || true
 pip install --upgrade -r requirements.txt  -i https://pypi.tuna.tsinghua.edu.cn/simple
